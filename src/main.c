04099ec69c13e0559e418943b80cc1a30a33c111
tesaddict <lefty.amperiadis@protonmail.com>
2023-09-09 11:02:15 -0500

    Added shifts and masks and re-worked mcp2515

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdbool.h>
 
 #include "mcp2515.h"
 #include "atmega328p_spi.h"
@@ -7,39 +8,59 @@
 
 #define MYUBRR F_CPU/16/BAUD-1
 
-#define TEST_ID 42U
-#define TEST_ARRAY_SZ 8U
-static const uint8_t TEST_ARRAY_OUT[TEST_ARRAY_SZ] = "12345678";
-static uint8_t TEST_ARRAY_IN[TEST_ARRAY_SZ] = { 0 };
+#define TEST_FRAME_ID 42U
+#define TEST_FRAME_SZ 8U
+#define TEST_FRAME_DATA { 1, 2, 3, 4, 5, 6, 7, 8 }
+
+#define TEST_MAX_ATTEMPTS 5
+
+static const mcp2515_frame_t TEST_FRAME_OUT = {
+  .id = TEST_FRAME_ID,
+  .size = TEST_FRAME_SZ,
+  .data = TEST_FRAME_DATA,
+};
+
+static mcp2515_frame_t TEST_FRAME_IN = { 0 };
 
 void usart_init(void);
 void usart_send(const char *data);
 
+static void test_print_status(const char *str, bool cond) {
+  if (cond) {
+    logger(INFO, "PASSED: ");
+  } else {
+    logger(INFO, "FAILED: ");
+  }
+  logger(INFO, str);
+  logger(INFO, "\r\n");
+}
+
 int main(void) {
   logger_init(&usart_init, &usart_send, DEBUG);
   atmega328p_spi_init();
-  mcp2515_init(KBPS_125, LOOPBACK, &atmega328p_spi_write, &atmega328p_spi_read);
-  logger(INFO, "PASSED: Initialization complete.\r\n");
-  mcp2515_set_id(TEST_ID);
-  mcp2515_send(TEST_ARRAY_OUT, TEST_ARRAY_SZ);
-  logger(INFO, "PASSED: Standard CAN frame message has been sent.\r\n");
-  uint8_t size = 0;
-  int32_t id = mcp2515_recv(TEST_ARRAY_IN, &size);
-  if (id != -1) {
-      logger(INFO, "PASSED: Received CAN frame message.\r\n");
-  } else {
-      logger(INFO, "FAILED: No CAN frame message has been received.\r\n");
+  mcp2515_init(KBPS_125, LOOPBACK, &atmega328p_spi_transact);
+  mcp2515_send(&TEST_FRAME_OUT);
+
+  // There is a delay from sending a frame to receiving it.
+  MCP2515_Status_t status;
+  for(uint8_t attempts = 0; attempts < TEST_MAX_ATTEMPTS; attempts++) {
+    status = mcp2515_recv(&TEST_FRAME_IN);
+    if (status == MCP2515_STATUS_SUCCESS) break;
   }
-  if (TEST_ARRAY_IN[0] == TEST_ARRAY_OUT[0]) {
-    logger(INFO, "PASSED: Sent data matches received data.\r\n");
-  } else {
-    logger(INFO, "FAILED: Sent data does not match received data.\r\n");
+
+  test_print_status("Receiving CAN frame message", (status == MCP2515_STATUS_SUCCESS));
+  test_print_status("Sent ID matches received ID", (TEST_FRAME_IN.id == TEST_FRAME_ID));
+  test_print_status("Sent size matches received size", (TEST_FRAME_OUT.size == TEST_FRAME_IN.size));
+
+  bool pass = false;
+  for (uint8_t idx = 0; idx < TEST_FRAME_IN.size; idx++) {
+    if (TEST_FRAME_OUT.data[idx] != TEST_FRAME_IN.data[idx]) {
+      pass = false;
+      break;
+    }
+    pass = true;
   }
-  if (id == 42) {
-    logger(INFO, "PASSED: Sent id matches received id.\r\n");
-  } else {
-    logger(INFO, "FAILED: Sent id does not match received id.\r\n");
-  }
+  test_print_status("Sent data matches received data", pass);
   return 0;
 }
 
--- src/mcp2515.c
+++ src/mcp2515.c
@@ -1,55 +1,48 @@
 #include "mcp2515.h"
-#include <stdint.h>
-
+#include "mcp2515_defines.h"
 
 static mcp2515_config_t mcp2515;
 
-#define MCP2515_SEND_SZ 16U
-static uint8_t out[MCP2515_SEND_SZ] = {
-  CAN_WRITE, TXB0CTRL,
-  0x00U, 0x00U,
-  0x00U, 0x00U,
-  0x00U, 0x00U,
-  0x00U, 0x00U,
-  0x00U, 0x00U,
-  0x00U, 0x00U,
-  0x00U, 0x00U
-};
-  
-#define MCP2515_RX_BUFFER_SZ 15U
-static uint8_t in[MCP2515_RX_BUFFER_SZ] = { 0xFFU };
+#define BUFFER_SIZE   14U
 
-static mcp2515_spi_write_t g_write;
-static mcp2515_spi_read_t g_read;
+#define COMMAND_INDEX 0U
+#define SIDH_INDEX    1U
+#define SIDL_INDEX    2U
+#define EID8_INDEX    3U
+#define EID0_INDEX    4U
+#define DLC_INDEX     5U
+#define DATA_INDEX    6U
+static uint8_t transmit_buffer[BUFFER_SIZE] = { 0x00U };
+static uint8_t receive_buffer[BUFFER_SIZE]  = { 0x00U };
+
+static mcp2515_spi_transact_func_ptr spi_transact;
 
 static void mcp2515_reset(void) {
   #define MCP2515_RESET_SZ 1U
-  const uint8_t cmd[MCP2515_RESET_SZ] = { CAN_RESET };
-  g_write(cmd, MCP2515_RESET_SZ);
+  uint8_t cmd[MCP2515_RESET_SZ] = { MCP2515_RESET };
+  spi_transact(cmd, MCP2515_RESET_SZ);
 }
 
 static void mcp2515_write(const uint8_t address, const uint8_t value) {
   #define MCP2515_WRITE_SZ 3U
-  const uint8_t cmd[MCP2515_WRITE_SZ] = { CAN_WRITE, address, value };
-  g_write(cmd, MCP2515_WRITE_SZ);
+  uint8_t cmd[MCP2515_WRITE_SZ] = { MCP2515_WRITE, address, value };
+  spi_transact(cmd, MCP2515_WRITE_SZ);
 }
 
 static uint8_t mcp2515_read(const uint8_t address) {
   #define MCP2515_READ_SZ 3U
-  uint8_t cmd[MCP2515_READ_SZ] = { CAN_READ, address, 0xFF };
-  g_read(cmd, MCP2515_READ_SZ);
+  uint8_t cmd[MCP2515_READ_SZ] = { MCP2515_READ, address, 0xFF };
+  spi_transact(cmd, MCP2515_READ_SZ);
   return cmd[2];
 }
 
-void mcp2515_init(const CAN_SPEED speed, 
-                  const CAN_MODE mode, 
-                  mcp2515_spi_write_t write,
-                  mcp2515_spi_read_t read) 
+void mcp2515_init(const MCP2515_SPEED speed, 
+                  const MCP2515_MODE mode, 
+                  mcp2515_spi_transact_func_ptr transact) 
 {
   mcp2515.speed = speed;
   mcp2515.mode  = mode;
-  g_write = write;
-  g_read = read;
+  spi_transact = transact;
   mcp2515_reset();
   /* TODO: Create a function that determines CNF1, CNF2 and CNF3 given a desired bit rate.
    * Assuming F_CPU = 16000000
@@ -63,42 +56,40 @@ void mcp2515_init(const CAN_SPEED speed,
    * TQ = 2 * (BRP + 1) / F_CPU
    * BRP = ((TQ * F_CPU) / 2) - 1 = 7
   */
-  mcp2515_write(CNF1, (1 << SJW0) | (1 << BRP0) | (1 << BRP1) | (1 << BRP2));
-  mcp2515_write(CNF2, (1 << BTLMODE) | (1 << PHSEG11));
-  mcp2515_write(CNF3, (1 << PHSEG21));
-  mcp2515_write(CANINTE, 0x00);
-  mcp2515_write(CANCTRL, (mcp2515.mode << 5));
-  mcp2515_write(RXB0CTRL, (1 << 6) | (1 << 5));
+  mcp2515_write(MCP2515_CNF1, MCP2515_CNF1_SJW0_MASK | MCP2515_CNF1_BRP0_MASK |
+                              MCP2515_CNF1_BRP1_MASK | MCP2515_CNF1_BRP2_MASK);
+  mcp2515_write(MCP2515_CNF2, MCP2515_CNF2_BTLMODE_MASK | MCP2515_CNF2_PHSEG11_MASK);
+  mcp2515_write(MCP2515_CNF3, MCP2515_CNF3_PHSEG21_MASK);
+  mcp2515_write(MCP2515_CANINTE, 0x00);
+  mcp2515_write(MCP2515_CANCTRL, (mcp2515.mode << MCP2515_CANCTRL_REQOP0_SHIFT));
+  mcp2515_write(MCP2515_RXB0CTRL, MCP2515_RXB0CTRL_RXM1_MASK | MCP2515_RXB0CTRL_RXM0_MASK);
 }
 
-void mcp2515_send(const uint8_t *data, const uint8_t size) {
-  out[7]  = size & 0x0FU;
-  for (uint8_t i = 0; i < size; ++i) {
-    out[8 + i] = data[i];
+void mcp2515_send(const mcp2515_frame_t *in) {
+  transmit_buffer[COMMAND_INDEX] = MCP2515_WRITE_TX_BUFFER_TXB0SIDH;
+  transmit_buffer[SIDH_INDEX] = (in->id & 0x07ff) >> 3;
+  transmit_buffer[SIDL_INDEX] = (in->id & 0x0007) << 5;
+  transmit_buffer[EID8_INDEX] = 0x00U;
+  transmit_buffer[EID0_INDEX] = 0x00U;
+  transmit_buffer[DLC_INDEX]  = in->size & 0x0FU;
+  for (uint8_t idx = 0; idx < in->size; ++idx) {
+    transmit_buffer[DATA_INDEX + idx] = in->data[idx];
   }
-  g_write(out, MCP2515_SEND_SZ);
-  mcp2515_write(out[1], (1 << TXREQ) | (1 << TXP1) | (1 << TXP0));
+  spi_transact(transmit_buffer, BUFFER_SIZE);
+  mcp2515_write(MCP2515_TXB0CTRL, MCP2515_TXBNCTRL_TXREQ_MASK | MCP2515_TXBNCTRL_TXP1_MASK | 
+                                  MCP2515_TXBNCTRL_TXP0_MASK);
 }
 
-#define MCP2515_SIDH_INDEX    1U
-#define MCP2515_SIDL_INDEX    2U
-#define MCP2515_EID8_INDEX    3U
-#define MCP2515_EID0_INDEX    4U
-#define MCP2515_DLC_INDEX     5U
-#define MCP2515_DATA_INDEX    6U
-int32_t mcp2515_recv(uint8_t *data, uint8_t *size) {
-  if (!(mcp2515_read(CANINTF) & (1 << RX0IF))) return -1;
-  in[0] = CAN_READ_RX_BUFFER_RXB0SIDH;
-  g_read(in, MCP2515_RX_BUFFER_SZ);
-  int32_t id = (in[MCP2515_SIDH_INDEX] << 3) | (in[MCP2515_SIDL_INDEX] >> 5);
-  *size = in[MCP2515_DLC_INDEX] & 0x0FU;
-  for (uint8_t i = 0; i < *size; ++i) {
-    data[i] = in[MCP2515_DATA_INDEX + i];
+MCP2515_Status_t mcp2515_recv(mcp2515_frame_t *out) {
+  if (!(mcp2515_read(MCP2515_CANINTF) & MCP2515_CANINTF_RX0IF_MASK)) {
+    return MCP2515_STATUS_RECEPTION_FAILED;
   }
-  return id;
-}
-
-void mcp2515_set_id(uint16_t id) {   
-  out[3] = (id & 0x07ff) >> 3;
-  out[4] = (id & 0x0007) << 5;
+  receive_buffer[COMMAND_INDEX] = MCP2515_READ_RX_BUFFER_RXB0SIDH;
+  spi_transact(receive_buffer, BUFFER_SIZE);
+  out->id = (receive_buffer[SIDH_INDEX] << 3) | (receive_buffer[SIDL_INDEX] >> 5);
+  out->size = receive_buffer[DLC_INDEX] & 0x0FU;
+  for (uint8_t idx = 0; idx < out->size; ++idx) {
+    out->data[idx] = receive_buffer[DATA_INDEX + idx];
+  }
+  return MCP2515_STATUS_SUCCESS;
 }
