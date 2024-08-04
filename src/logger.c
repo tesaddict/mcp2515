944e84ef85e1eae64c86c0a140754a7a2546f1ca
tesaddict <lefty.amperiadis@proton.me>
2024-08-04 13:48:35 -0500

    Re-worked logger to allow adding new printing forms generically

--- src/logger.c
+++ src/logger.c
@@ -1,7 +1,5 @@
 #include "logger.h"
 
-static const char hex[16] = { '0', '1', '2', '3', '4', '5', '6', '7', 
-                              '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
 typedef struct {
   void (*open)  (void);
@@ -25,22 +23,26 @@ void logger(const LOG_LEVEL level, const char *data) {
   }
 }
 
+static const char HEX_ARRAY[16] = { '0', '1', '2', '3', '4', '5', '6', '7', 
+                                    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+#define TEMP_ARRAY_UPPER 2U
+#define TEMP_ARRAY_LOWER 3U
+static char print_array[] = { '0', 'x', '\0', '\0', ' ' };
+static const size_t PRINT_ARRAY_SIZE = sizeof(print_array) / sizeof(char);
 static void byte_to_hex_string(uint8_t byte, char **dest) {
-  uint8_t upper = (byte & 0xf0) >> 4; 
-  uint8_t lower = (byte & 0x0f);
+  print_array[TEMP_ARRAY_UPPER] = HEX_ARRAY[(byte & 0xf0) >> 4];
+  print_array[TEMP_ARRAY_LOWER] = HEX_ARRAY[(byte & 0x0f)];
   char *dest_ptr = *dest;
-  *dest_ptr       = '0';
-  *(dest_ptr + 1) = 'x';
-  *(dest_ptr + 2) = hex[upper];
-  *(dest_ptr + 3) = hex[lower];
-  *(dest_ptr + 4) = ' ';
-  *dest += 5;
+  for (size_t idx = 0; idx < PRINT_ARRAY_SIZE; idx++) {
+    dest_ptr[idx] = print_array[idx];
+  }
+  *dest += PRINT_ARRAY_SIZE;
 }
 
 #define LOG_BUFFER_SIZE 255U
 static char log_buffer[LOG_BUFFER_SIZE];
 void logger_print_bytes_as_hex(const uint8_t *bytes, const uint8_t size) {
-  if (size == 0U || ((size * 5) + 2 >= LOG_BUFFER_SIZE)) {
+  if (size == 0U || (size * PRINT_ARRAY_SIZE + 2 >= LOG_BUFFER_SIZE)) {
     return;
   }
   char *dest = log_buffer;
